1e7cc9b5b7647f4967ff87cc31abb260d279135f
Javier Cervantes <1.27201@gmail.com>
2015-08-26 17:05:44 -0500

    Remove test unit

--- test/unit/configuration_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test/unit'
-require "configuration"
-
-class SettingsTest < Test::Unit::TestCase
-  def test_we_can_read_config_file
-    assert_not_nil Configuration
-  end
-end
--- test/unit/xml_writer_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-require 'xml_writer'
-
-class XMLWriterTest < Test::Unit::TestCase
-  def test_write
-    file = XMLWriter.new.write
-    assert_not_nil file, "writing XML File failed"
-  end
-end
