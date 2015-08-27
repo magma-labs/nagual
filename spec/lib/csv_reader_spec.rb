6504a6e31199ed044703f0f08b41968f737a88fa
Javier Cervantes <1.27201@gmail.com>
2015-08-27 11:47:31 -0500

    Refactoring spec data parsed_content to shared context

--- spec/lib/csv_reader_spec.rb
+++ spec/lib/csv_reader_spec.rb
@@ -3,18 +3,14 @@ require 'csv_reader'
 
 RSpec.describe CSVReader do
 
+  include_context 'data'
+
   let(:file) { CSVReader.new.read }
-  let(:expected_result) do
-    [
-      { nombre_autor: 'Rigo Tovar', cancion:  'El Testamento' },
-      { nombre_autor: 'Chico Che',  cancion:  'Quen Pompo' }
-    ]
-  end
 
   describe 'read' do
 
     it 'returns expected result' do
-      expect(subject.read).to eq(expected_result)
+      expect(subject.read).to eq(parsed_content)
     end
 
   end
---
+++ spec/support/shared_context.rb
@@ -0,0 +1,10 @@
+RSpec.shared_context 'data' do
+
+  let(:parsed_content) do
+    [
+      { nombre_autor: 'Rigo Tovar', cancion:  'El Testamento' },
+      { nombre_autor: 'Chico Che',  cancion:  'Quen Pompo' }
+    ]
+  end
+
+end
