7f4bfd08f7f510e70884f5c3fbef7e8ae385f1df
Javier Cervantes <1.27201@gmail.com>
2015-08-28 10:46:52 -0500

    Adding missing reader tests

--- lib/nagual/reader.rb
+++ lib/nagual/reader.rb
@@ -1,5 +1,7 @@
-class Reader
-  def read
-    raise NotImplementedError, 'Ask the subclass'
+module Nagual
+  class Reader
+    def read
+      raise NotImplementedError, 'Ask the subclass'
+    end
   end
 end
