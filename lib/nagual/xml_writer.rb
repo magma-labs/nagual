eabde2d8034c8845fae48576f94616f351a5fa6a
Javier Cervantes <1.27201@gmail.com>
2015-08-31 10:40:15 -0500

    Refactoring xml_writer

--- lib/nagual/xml_writer.rb
+++ lib/nagual/xml_writer.rb
@@ -5,22 +5,21 @@ module Nagual
 
     def initialize(content)
       @elements = content
-      @label    = 'product'
     end
 
     def write
       Nokogiri::XML::Builder.new do |xml|
         xml.catalog {
-          process_array(xml)
+          add_node('product', xml)
         }
       end.to_xml
     end
 
     private
 
-    def process_array(xml)
+    def add_node(label, xml)
       @elements.each do |hash|
-        xml.send(@label, hash[:attributes]) do
+        xml.send(label, hash[:attributes]) do
           hash.each do |key, value|
             if key != :attributes
               xml.send(key, value)
