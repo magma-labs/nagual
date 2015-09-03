fc2d9cf26b98f13843cec6a2736847922f810822
Javier Cervantes <1.27201@gmail.com>
2015-09-03 14:53:45 -0500

    Removing extra before block

--- spec/nagual/database_spec.rb
+++ spec/nagual/database_spec.rb
@@ -2,11 +2,8 @@ require 'spec_helper'
 
 RSpec.describe Nagual::Database do
 
-  before do
-  end
-
   it 'loads a csv file' do
-    described_class.load('tests')
+    expect(described_class.load('tests')).not_to be_empty
   end
 
 end
