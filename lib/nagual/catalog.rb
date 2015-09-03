9716211e38adfdf1d36c780cf8bd9eee163d2495
Javier Cervantes <1.27201@gmail.com>
2015-09-03 14:53:57 -0500

    Refactoring catalog to make it more readable

--- lib/nagual/catalog.rb
+++ lib/nagual/catalog.rb
@@ -2,37 +2,37 @@ module Nagual
   class Catalog
 
     def initialize
-      content    = Database.load('products')
-      groups     = Database.load('image_groups')
-      images     = Database.load('images')
-      attributes = Database.load('page_attributes')
-      products   = Database.load('bundled_products')
-      sets       = Database.load('product_set_products')
-      options    = Database.load('options')
-      values     = Database.load('option_values')
-      prices     = Database.load('option_value_prices')
-      links      = Database.load('product_links')
+      groups      = Database.load('image_groups')
+      options     = Database.load('options')
+      values      = Database.load('option_values')
 
-      groups  = CSV.add_children(groups, :images, images, :image)
-      values  = CSV.add_children(values, :'option-value-prices', prices,
-                                 :'option-value-price')
-      options = CSV.add_children(options, :'option-values', values,
-                                 :'option-value')
-
-      content = CSV.add_children(content, :images, groups, :'image-group')
-      content = CSV.add_children(content, :'page-attributes', attributes)
-      content = CSV.add_children(content, :'bundled-products', products,
-                                 :'bundled-product')
-      content = CSV.add_children(content, :'product-set-products', sets,
-                                 :'product-set-product')
-      content = CSV.add_children(content, :'options', options, :option)
-      content = CSV.add_children(content, :'product-links', links,
-                                 :'product-link')
-      @xml = XML.new(content, 'catalog', 'product', catalog_attributes)
+      @products   = Database.load('products')
+      @images     = Database.load('images')
+      @attributes = Database.load('page_attributes')
+      @bundled    = Database.load('bundled_products')
+      @sets       = Database.load('product_set_products')
+      @prices     = Database.load('option_value_prices')
+      @links      = Database.load('product_links')
+      @groups     = CSV.add_children(groups, :images, @images, :image)
+      @values     = CSV.add_children(values, :'option-value-prices', @prices,
+                                     :'option-value-price')
+      @options    = CSV.add_children(options, :'option-values', @values,
+                                     :'option-value')
     end
 
     def to_xml
-      @xml.build
+      content = @products
+      content = CSV.add_children(content, :images, @groups, :'image-group')
+      content = CSV.add_children(content, :'page-attributes', @attributes)
+      content = CSV.add_children(content, :'bundled-products', @bundled,
+                                 :'bundled-product')
+      content = CSV.add_children(content, :'product-set-products', @sets,
+                                 :'product-set-product')
+      content = CSV.add_children(content, :'options', @options, :option)
+      content = CSV.add_children(content, :'product-links', @links,
+                                 :'product-link')
+
+      XML.new(content, 'catalog', 'product', catalog_attributes).build
     end
 
     private
