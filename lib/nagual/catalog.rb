11356795c3aa1c29e80238bdb9c6ab1f0619ff5f
Javier Cervantes <1.27201@gmail.com>
2015-09-03 11:44:54 -0500

    Add options

--- lib/nagual/catalog.rb
+++ lib/nagual/catalog.rb
@@ -8,15 +8,23 @@ module Nagual
       attributes = Database.load('page_attributes')
       products   = Database.load('bundled_products')
       sets       = Database.load('product_set_products')
+      options    = Database.load('options')
+      values     = Database.load('option_values')
+      prices     = Database.load('option_value_prices')
 
       groups  = CSV.add_children(groups, :images, images, :image)
+      values  = CSV.add_children(values, :'option-value-prices', prices,
+                                 :'option-value-price')
+      options = CSV.add_children(options, :'option-values', values,
+                                 :'option-value')
+
       content = CSV.add_children(content, :images, groups, :'image-group')
       content = CSV.add_children(content, :'page-attributes', attributes)
       content = CSV.add_children(content, :'bundled-products', products,
                                  :'bundled-product')
       content = CSV.add_children(content, :'product-set-products', sets,
                                  :'product-set-product')
-
+      content = CSV.add_children(content, :'options', options, :option)
       @xml = XML.new(content, 'catalog', 'product', catalog_attributes)
     end
 
