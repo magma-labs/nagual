2e39b30a281d9371dc67bad8d3a19fc90454bd0e
Javier Cervantes <javier@sawyereffect.com>
2016-01-21 12:29:19 -0600

    Update rubocop version and fix error

--- lib/nagual/product.rb
+++ lib/nagual/product.rb
@@ -2,21 +2,21 @@ require 'nokogiri'
 
 module Nagual
   class Product
-    ATTRIBUTES     = [:product_id, :mode]
+    ATTRIBUTES     = [:product_id, :mode].freeze
     FIRST_ELEMENTS = [
       :ean, :upc, :min_order_quantity, :step_quantity, :display_name,
       :short_description, :long_description, :online_flag, :online_from,
       :online_to, :searchable_flag, :searchable_if_unavailable_flag
-    ]
+    ].freeze
     LAST_ELEMENTS = [
       :template, :tax_class_id, :brand, :manufacturer_name, :manufacturer_sku,
       :search_placement, :search_rank, :sitemap_included_flag,
       :sitemap_changefrequency, :sitemap_priority
-    ]
+    ].freeze
 
     PAGE_ELEMENTS = [
       :page_title, :page_description, :page_keywords, :page_url
-    ]
+    ].freeze
 
     PROPERTIES = ATTRIBUTES + FIRST_ELEMENTS + LAST_ELEMENTS + PAGE_ELEMENTS
     PROPERTIES.each do |attribute|
--- nagual.gemspec
+++ nagual.gemspec
@@ -23,5 +23,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.10'
   spec.add_development_dependency 'simplecov', '~> 0.10'
   spec.add_development_dependency 'rspec', '~> 3.3'
-  spec.add_development_dependency 'rubocop', '~> 0.33'
+  spec.add_development_dependency 'rubocop', '~> 0.36'
 end
