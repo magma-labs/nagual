3a622af91528405e07515f40e8eb1f98ef0dc5de
Javier Cervantes <javier@sawyereffect.com>
2016-02-11 15:48:39 -0600

    Format changes

--- lib/nagual/api.rb
+++ lib/nagual/api.rb
@@ -10,8 +10,9 @@ module Nagual
 
     def review(input_path)
       logger.debug("CSV::Input to be created for #{input_path}")
-      input = CSV::Input.new(input_path)
+      input  = CSV::Input.new(input_path)
       report = "#{input.valid_products.count} valid products\n"
+
       add_invalid_information(report, input.invalid_products)
       report
     end
@@ -19,8 +20,10 @@ module Nagual
     def export(input_path)
       logger.debug("CSV::Input to be created for #{input_path}")
       input = CSV::Input.new(input_path)
-      logger.info("Catalog created with #{input.valid_products.count} products")
+
+      logger.info("Catalog to be created with #{input.valid_products.count} products")
       catalog = Catalog.new(input.valid_products)
+
       XML::Catalog.new(catalog).output
     end
 
