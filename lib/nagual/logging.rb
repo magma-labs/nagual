00012bff376044b24b7249caf66bbc2422e1b831
Javier Cervantes <javier@sawyereffect.com>
2016-02-11 10:45:52 -0600

    INFO logger level by default

--- lib/nagual/logging.rb
+++ lib/nagual/logging.rb
@@ -14,6 +14,7 @@ module Nagual
 
     def create_logger
       logger = Logger.new(STDOUT)
+      logger.level = Logger::INFO
       logger.formatter = proc do |severity, datetime, _progname, msg|
         date_format = datetime.strftime('%Y-%m-%d %H:%M:%S')
         "[#{date_format}] #{severity} (#{self.class}): #{msg}\n"
