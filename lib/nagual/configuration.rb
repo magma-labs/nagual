ea06cb3761a9504bddccb899d3f29bcadb138225
Javier Cervantes <javier@sawyereffect.com>
2016-02-11 16:19:25 -0600

    Avoid reading file more than once in configuration

--- lib/nagual/configuration.rb
+++ lib/nagual/configuration.rb
@@ -3,6 +3,10 @@ require 'yaml'
 module Nagual
   module Configuration
     def config
+      @config ||= load_config_from_file
+    end
+
+    def load_config_from_file
       path = File.join('./config/configuration.yml')
       YAML.load_file(path)
     end
