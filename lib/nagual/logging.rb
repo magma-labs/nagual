7c5140d20ebb3b93f91322d939b296450de0ec07
Javier Cervantes <javier@sawyereffect.com>
2016-02-11 16:50:47 -0600

    Use configuration to setup logger

--- lib/nagual/logging.rb
+++ lib/nagual/logging.rb
@@ -1,23 +1,22 @@
 require 'logger'
+require 'nagual/configuration'
 
 module Nagual
   module Logging
+    include Nagual::Configuration
+
     def logger
       @@logger ||= create_logger
-    end
-
-    def self.logger=(logger)
-      @@logger = logger
     end
 
     private
 
     def create_logger
-      logger = Logger.new(STDOUT)
-      logger.level = Logger::INFO
-      logger.formatter = proc do |severity, datetime, _progname, msg|
-        date_format = datetime.strftime('%Y-%m-%d %H:%M:%S')
-        "[#{date_format}] #{severity} (#{self.class}): #{msg}\n"
+      logger = Logger.new(File.new(config['log']['file'], 'a'))
+      logger.level = Logger.const_get config['log']['level']
+      logger.formatter = proc do |severity, datetime, progname, msg|
+        date_format = datetime.strftime(config['log']['date_format'])
+        "[#{date_format}] #{severity} [#{progname}]: #{msg}\n"
       end
       logger
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,14 +4,6 @@ SimpleCov.start
 
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
 
-class NullLogger < Logger
-  def initialize(*args)
-  end
-
-  def add(*args, &block)
-  end
-end
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -19,9 +11,5 @@ RSpec.configure do |config|
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
-  end
-
-  config.before(:suite) do
-    #Nagual::Logging.logger = NullLogger.new
   end
 end
