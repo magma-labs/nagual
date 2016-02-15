5b415d15e8bfa24bd30a9a851458da21a598c3e3
Javier Cervantes <javier@sawyereffect.com>
2016-02-15 13:02:17 -0600

    Input only reads rows

--- lib/nagual/input/csv.rb
+++ lib/nagual/input/csv.rb
@@ -9,61 +9,20 @@ module Nagual
       include Nagual::Logging
       include Nagual::Configuration
 
+      attr_reader :rows
+
       def initialize(file)
         logger.info('Input::CSV') { "Opening #{file} to read csv content" }
-        column_names, *rows = *::CSV.parse(File.read(file), headers: true)
-        logger.debug('Input::CSV') { "Column names extracted: #{column_names}" }
-        @content = rows.map { |row| Hash[column_names.zip(row)] }
-      end
-
-      def valid_products
-        products.select(&:valid?)
-      end
-
-      def invalid_products
-        products.select { |product| !product.valid? }
+        content = File.read(file)
+        @rows   = parse(content)
       end
 
       private
 
-      def products
-        @products || parse_content_to_products
-      end
-
-      def parse_content_to_products
-        logger.debug('Input::CSV') { 'Parsing content to products' }
-        @content.map do |attrs|
-          product = Nagual::Models::Product
-                    .new(attributes: product_attributes(attrs),
-                         variations: variations_for(attrs),
-                         images: images_for(attrs))
-          logger.debug('Input::CSV') { "Product created: #{product}" }
-          product
-        end
-      end
-
-      def product_attributes(attributes)
-        attributes.select do |key|
-          key != 'images' && !key.match(config['product']['variation_regex'])
-        end
-      end
-
-      def variations_for(attributes)
-        variation_attributes = attributes.select do |key|
-          key.to_s.match(config['product']['variation_regex'])
-        end
-
-        variation_attributes.map do |key, value|
-          id     = key.gsub(config['product']['variation_regex'], '')
-          values = value.split(',')
-
-          Nagual::Models::ProductVariation.new(id: id, values: values)
-        end
-      end
-
-      def images_for(attributes)
-        images = attributes['images'] || ''
-        images.split(',')
+      def parse(content)
+        column_names, *rows = *::CSV.parse(content, headers: true)
+        logger.debug('Input::CSV') { "Column names extracted: #{column_names}" }
+        rows.map { |row| Hash[column_names.zip(row)] }
       end
     end
   end
--- spec/nagual/input/csv_spec.rb
+++ spec/nagual/input/csv_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 require 'nagual/input/csv'
 
 RSpec.describe Nagual::Input::CSV do
-  let(:first_product) { subject.valid_products.first }
-
   subject { described_class.new('') }
 
   context 'with correct data' do
@@ -17,77 +15,10 @@ RSpec.describe Nagual::Input::CSV do
     end
 
     it 'returns array of valid products' do
-      expect(subject.valid_products.count).to eq(2)
-      expect(first_product.class).to eq(Nagual::Models::Product)
-      expect(first_product.variations).to eq([])
-    end
-
-    it 'has no invalid products' do
-      expect(subject.invalid_products.count).to eq(0)
-    end
-  end
-
-  context 'with invalid data' do
-    before do
-      allow(File)
-        .to receive(:read) do
-        "product_id,available_flag\n" \
-        "1234,true\n" \
-        "2345,NOT\n"
-      end
-    end
-
-    it 'returns array of valid products' do
-      expect(subject.valid_products.count).to eq(1)
-    end
-
-    it 'has no invalid products' do
-      expect(subject.invalid_products.count).to eq(1)
-    end
-  end
-
-  context 'with variations' do
-    before do
-      allow(File)
-        .to receive(:read) do
-        "product_id,ean,variation-color,variation-size\n" \
-          "1234, EAN1,\"Blue,Red\",\"Small,Large\"\n" \
-      end
-    end
-
-    it 'creates product variations correctly' do
-      variations = first_product.variations
-
-      expect(variations.size).to eq(2)
-      expect(variations.first.class).to eq(Nagual::Models::ProductVariation)
-    end
-  end
-
-  context 'with images' do
-    before do
-      allow(File)
-        .to receive(:read) do
-        "product_id,ean,images\n" \
-        "1234, EAN1,\"small,medium\"\n" \
-      end
-    end
-
-    it 'creates product variations correctly' do
-      expect(first_product.images).to eq(%w(small medium))
-    end
-  end
-
-  context 'with custom attributes' do
-    before do
-      allow(File)
-        .to receive(:read) do
-        "product_id,ean,other,custom-other\n" \
-        "1234,EAN1,ignore,value\n" \
-      end
-    end
-
-    it 'creates product variations correctly' do
-      expect(first_product.custom_attributes).to eq(other: 'value')
+      expect(subject.rows).to eq([
+        {'product_id' => '1234', 'ean' => 'EAN1'},
+        {'product_id' => '2345', 'ean' => 'EAN2'}
+      ])
     end
   end
 end
