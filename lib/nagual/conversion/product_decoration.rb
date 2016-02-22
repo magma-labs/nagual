5a3cbaeada2f6491b4f7419537b0181a70c378f9
Javier Cervantes <javier@sawyereffect.com>
2016-02-22 10:42:56 -0600

    Adding copy and merge decorations
    With copy you can copy the value of a column and with merge you can use
    additional keys and enrich the copied data with a pattern

--- lib/nagual/conversion/product_decoration.rb
+++ lib/nagual/conversion/product_decoration.rb
@@ -10,13 +10,38 @@ module Nagual
       end
 
       def build
-        @row.merge(fixed_values)
+        @row.merge(fixed_values).merge(copied_values).merge(merge_values)
       end
 
       private
 
       def fixed_values
-        config['decoration']['product']['values']
+        product_config['fixed']
+      end
+
+      def copied_values
+        product_config['copy']
+          .select { |copy| @row[copy['key']] }
+          .map    { |copy| [copy['to'], @row[copy['key']]] }
+          .to_h
+      end
+
+      def merge_values
+        product_config['merge'].map do |merge|
+          values = prepare_values(merge['keys'])
+          [merge['to'], merge['pattern'] % values] unless values.empty?
+        end.compact.to_h
+      end
+
+      def prepare_values(keys)
+        @row
+          .select { |key, _| keys.include?(key) }
+          .map    { |key, value| [key.to_sym, value] }
+          .to_h
+      end
+
+      def product_config
+        config['decoration']['product']
       end
     end
   end
--- spec/nagual/conversion/product_decoration_spec.rb
+++ spec/nagual/conversion/product_decoration_spec.rb
@@ -4,12 +4,34 @@ require 'nagual/conversion/product_decoration'
 RSpec.describe Nagual::Conversion::ProductDecoration do
   subject { described_class.new(row) }
 
+  let(:fixed_values) do
+    { 'min_order_quantity' => 10, 'available_flag' => 'true' }
+  end
+  let(:expected) { fixed_values.merge(row) }
+
   context 'for fixed values' do
     let(:row) { {} }
 
     it 'adds as expected' do
-      expect(subject.build).to eq('min_order_quantity' => 10,
-                                  'available_flag' => 'true')
+      expect(subject.build).to eq(expected)
+    end
+  end
+
+  context 'for copy values' do
+    let(:row) { { 'short_description' => 'nice description' } }
+
+    it 'adds as expected' do
+      expect(subject.build).to eq(expected.merge(
+                                    'page_description' => 'nice description'))
+    end
+  end
+
+  context 'for merge values' do
+    let(:row) { { 'product_id' => 'id', 'organization' => 'sawyer' } }
+
+    it 'adds as expected' do
+      expect(subject.build).to eq(expected.merge(
+                                    'page_url' => 'http://host/sawyer/id'))
     end
   end
 end
