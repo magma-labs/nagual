97d8daef91c920701a18c67582f013be5a61f772
Javier Cervantes <javier@sawyereffect.com>
2016-03-01 09:53:07 -0600

    Add convert option to pass values as they are instead of default

--- lib/nagual/conversion/product_mapping.rb
+++ lib/nagual/conversion/product_mapping.rb
@@ -27,8 +27,14 @@ module Nagual
         case name
         when 'none' then value
         when 'convert'
-          params['values'][value] || params['default']
+          convert(name, value, params)
         end
+      end
+
+      def convert(_name, value, params)
+        return params['values'][value] if params['values'][value]
+
+        params['default'] == 'NONE' ? value : params['default']
       end
     end
   end
--- spec/nagual/conversion/product_mapping_spec.rb
+++ spec/nagual/conversion/product_mapping_spec.rb
@@ -2,59 +2,80 @@ require 'spec_helper'
 require 'nagual/conversion/product_mapping'
 
 RSpec.describe Nagual::Conversion::ProductMapping do
-  let(:mutations) do
-    [
-      { 'key' => 'id', 'to' => 'product_id', 'name' => 'none' },
-      { 'key' => 'status', 'to' => 'online_flag', 'name' => 'convert',
-        'params' => {
-          'default' => 'false',
-          'values'  => { 'online' => 'true' }
-        } }
-    ]
-  end
-
-  subject { described_class.new(row, mutations) }
-
-  context 'with simple mapped values' do
-    let(:row) { { 'id' => 'id' } }
-
-    it 'has no errors' do
-      expect(subject.transform).to eq('product_id' => 'id')
+  context 'with default' do
+    let(:mutations) do
+      [
+        { 'key' => 'id', 'to' => 'product_id', 'name' => 'none' },
+        { 'key' => 'status', 'to' => 'online_flag', 'name' => 'convert',
+          'params' => {
+            'default' => 'false',
+            'values'  => { 'online' => 'true' }
+          } }
+      ]
     end
-  end
 
-  context 'with empty values' do
-    let(:row) { { 'id' => nil } }
+    subject { described_class.new(row, mutations) }
 
-    it 'removes value' do
-      expect(subject.transform).to eq({})
+    context 'with simple mapped values' do
+      let(:row) { { 'id' => 'id' } }
+
+      it 'has no errors' do
+        expect(subject.transform).to eq('product_id' => 'id')
+      end
     end
-  end
 
-  context 'with not simple mapped values' do
-    let(:row) { { 'non-existent': 'value' } }
+    context 'with empty values' do
+      let(:row) { { 'id' => nil } }
 
-    it 'has no errors' do
-      expect(subject.transform).to eq({})
+      it 'removes value' do
+        expect(subject.transform).to eq({})
+      end
     end
-  end
 
-  context 'with mutation values' do
-    context 'boolean' do
-      context 'with expected value' do
-        let(:row) { { 'status' => 'online' } }
+    context 'with not simple mapped values' do
+      let(:row) { { 'non-existent': 'value' } }
 
-        it 'returns true' do
-          expect(subject.transform).to eq('online_flag' => 'true')
+      it 'has no errors' do
+        expect(subject.transform).to eq({})
+      end
+    end
+
+    context 'with mutation values' do
+      context 'boolean' do
+        context 'with expected value' do
+          let(:row) { { 'status' => 'online' } }
+
+          it 'returns true' do
+            expect(subject.transform).to eq('online_flag' => 'true')
+          end
+        end
+
+        context 'with other value' do
+          let(:row) { { 'status' => 'offline' } }
+
+          it 'returns true' do
+            expect(subject.transform).to eq('online_flag' => 'false')
+          end
         end
       end
+    end
 
-      context 'with other value' do
-        let(:row) { { 'status' => 'offline' } }
+    context 'without default' do
+      let(:mutations) do
+        [
+          { 'key' => 'status', 'to' => 'online_flag', 'name' => 'convert',
+            'params' => {
+              'default' => 'NONE',
+              'values'  => { 'online' => 'true' }
+            } }
+        ]
+      end
+      let(:row) { { 'status' => 'offline' } }
 
-        it 'returns true' do
-          expect(subject.transform).to eq('online_flag' => 'false')
-        end
+      subject { described_class.new(row, mutations) }
+
+      it 'returns same value' do
+        expect(subject.transform).to eq('online_flag' => 'offline')
       end
     end
   end
